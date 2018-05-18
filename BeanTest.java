19d378c91a94c1956b28bf72ee6f35abac2ed637
Reedzxb <634229452@qq.com>
2018-05-18 12:28:11 +0800

    Struts

---
+++ BeanTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Description:
+ * <br/>��վ: <a href="http://www.crazyit.org">���Java����</a>
+ * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author  Yeeku.H.Lee kongyeeku@163.com
+ * @version  1.0
+ */
+public class BeanTest
+{
+	public static void main(String[] args)
+		throws Exception
+	{
+
+	}
+}
